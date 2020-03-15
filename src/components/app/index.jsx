04d67b865d2444f7f45bcc08d807a5f6b8096c5e
Jens <jede.87@gmail.com>
2020-03-15 17:06:19 +0100

    added eslint airbnb and react reveal

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,44 +1,60 @@
-import React from "react";
-import "./app.scss";
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+import './app.scss';
 
 export default () => {
   return (
     <>
-      <header>
-        <div className="container">
-          <div className="row">
-            <div className="col-10">
-              <ul class="list-inline mt-3 mb-0">
-                <li class="list-inline-item h3 mr-4">Consultant</li>
-                <li class="list-inline-item h3 mr-4">Performer</li>
-                <li class="list-inline-item h3">Athlete</li>
-              </ul>
-            </div>
-            <div className="col-2 text-right">
-              <h1 className="mt-2 mb-1">JK</h1>
+      <header className="container">
+        <div className="row">
+          <div className="col-12 mt-5">
+            <div className="text-center">
+              <h1 className="display-1 mb-0">Jens Klemets</h1>
+              <p className="lead">developer | techno dj</p>
             </div>
           </div>
         </div>
       </header>
-      <main className="h-100 container">
+      <main className="container mt-5">
         <div className="row">
-          <div className="col-12 d-flex h-100 mt-5">
-            <div class="card mt-2">
-              <div class="card-body">
-                <h2 className="display-2 text-md-center text-right">
-                  Jens Klemets
-                </h2>
-                <p>
-                  Etiam nec pretium dui. Maecenas accumsan aliquet ligula,
-                  vulputate aliquam augue bibendum vitae. Donec ligula metus,
-                  tincidunt at elementum eu, condimentum ut neque. Quisque quis
-                  felis suscipit tortor malesuada consequat. Maecenas fermentum
-                  est efficitur ullamcorper venenatis. Quisque volutpat lorem
-                  dolor, quis pulvinar metus pharetra a. Duis at ornare sem, id
-                  blandit tellus. Donec nec convallis tellus. Nullam sagittis
-                  lobortis facilisis. Vestibulum in maximus augue.
-                </p>
+          <div className="col-6">
+            <Fade left delay={600}>
+              <div className="card mt-2">
+                <div className="card-body">
+                  <h2 className="text-center display-3">Developer</h2>
+                  <p className="text-center lead">
+                    Jens is a great front end developer and has over 10 years of
+                    documented experience. He has experience from a wide range
+                    of projects.
+                  </p>
+                  <button
+                    className="btn btn-primary btn-lg btn-block"
+                    onClick={() => console.log('go to linkedin')}
+                  >
+                    LinkedIn
+                  </button>
+                </div>
               </div>
+            </Fade>
+          </div>
+          <div className="col-6">
+            <div className="card mt-2">
+              <Fade right delay={1000}>
+                <div className="card-body">
+                  <h2 className="text-center display-3">DJ</h2>
+                  <p className="text-center lead">
+                    Jens has a broad taste in music and do all types of events.
+                    He has his own techno show on SoundCloud playing his latest
+                    findings.
+                  </p>
+                  <button
+                    className="btn btn-warning btn-lg btn-block"
+                    onClick={() => console.log('go to soundcloud')}
+                  >
+                    SoundCloud
+                  </button>
+                </div>
+              </Fade>
             </div>
           </div>
         </div>
